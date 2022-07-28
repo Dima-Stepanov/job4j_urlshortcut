ad96efb0971c4d2aff7a547b578ee60ae142074f
dmitryjob4j <haoos@inbox.ru>
2022-07-28 18:20:36 +0300

    Create model and Pass generate

---
+++ src/main/java/ru/job4j/urlshortcut/domain/Site.java
@@ -0,0 +1,84 @@
+package ru.job4j.urlshortcut.domain;
+
+import java.util.Objects;
+
+/**
+ * 3. Мидл
+ * 3.4. Spring
+ * 3.4.9.2.Контрольные вопросы
+ * 2. Сервис - UrlShortCut [#13799]
+ * Site domain модель описывает сайты зарегистрированные в системе
+ *
+ * @author Dmitry Stepanov, user Dmitry
+ * @since 28.07.2022
+ */
+public class Site {
+    private int id;
+    private String login;
+    private String password;
+    private boolean registration;
+
+    public static Site of(String login, String password, boolean registration) {
+        Site site = new Site();
+        site.login = login;
+        site.password = password;
+        site.registration = registration;
+        return site;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(boolean registration) {
+        this.registration = registration;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Site site = (Site) o;
+        return id == site.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Site{id=" + id + ", login='" + login + '\''
+                + ", password='" + password + '\''
+                + ", registration=" + registration + '}';
+    }
+}
---
+++ src/main/java/ru/job4j/urlshortcut/domain/UrlSite.java
@@ -0,0 +1,94 @@
+package ru.job4j.urlshortcut.domain;
+
+
+import java.util.Objects;
+
+/**
+ * 3. Мидл
+ * 3.4. Spring
+ * 3.4.9.2.Контрольные вопросы
+ * 2. Сервис - UrlShortCut [#13799]
+ * Url domain модель URL ссылки добавленные в систему для кодировки.
+ *
+ * @author Dmitry Stepanov, user Dmitry
+ * @since 28.07.2022
+ */
+public class UrlSite {
+    private int id;
+    private String url;
+    private String code;
+    private Site site;
+    private int total;
+
+    public static UrlSite of(String url, String code, Site site, int total) {
+        UrlSite urlSite = new UrlSite();
+        urlSite.url = url;
+        urlSite.code = code;
+        urlSite.site = site;
+        return urlSite;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Site getSite() {
+        return site;
+    }
+
+    public void setSite(Site site) {
+        this.site = site;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UrlSite urlSite = (UrlSite) o;
+        return id == urlSite.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "UrlSite{id=" + id + ", url='" + url + '\''
+                + ", code='" + code + '\'' + ", site=" + site
+                + ", total=" + total + '}';
+    }
+}
